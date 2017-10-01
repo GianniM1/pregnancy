fd1e953b36074ea2b61e4e9d39cf6f7e971b43f6
Gianni Martinez <giannimartinez@Giannis-MacBook-Air.local>
2017-10-01 17:01:01 -0400

    Added source code to unzip files and download them

--- fakeTest.php
+++ fakeTest.php
@@ -1,47 +1,46 @@
 <?php
+// send POST request to JSON placeholder
 
-$url = 'https://jsonplaceholder.typicode.com';
-$urlParam = "/posts";
-
-
-$data = array("title" => "foo", "body" => "bar", "userId" => "1");
-$data_string = json_encode($data);
-
-$ch = curl_init($url.$urlParam);
-
-
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string))
-);
-
-$result = curl_exec($ch);
-echo $result;
-
-// POST adds a random id to the object sent
-// $.ajax('http://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   data: {
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1
-//   }
-// }).then(function(data) {
-//   console.log(data);
-// });
+// $url = 'https://jsonplaceholder.typicode.com';
+// $urlParam = "/posts";
 //
-// /* will return
-// {
-//   id: 101,
-//   title: 'foo',
-//   body: 'bar',
-//   userId: 1
-// }
-// */
+//
+// $data = array("title" => "foo", "body" => "bar", "userId" => "1");
+// $data_string = json_encode($data);
+//
+// $ch = curl_init($url.$urlParam);
+//
+//
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//     'Content-Type: application/json',
+//     'Content-Length: ' . strlen($data_string))
+// );
+//
+// $result = curl_exec($ch);
+// echo $result;
 
+//download zip file
+// $url = "http://ipv4.download.thinkbroadband.com/5MB.zip";
+// $zipIntilize = curl_init();
+// curl_setopt($zipIntilize, CURLOPT_URL, $url);
+// curl_setopt($zipIntilize, CURLOPT_RETURNTRANSFER, 1);
+// $data = curl_exec ($zipIntilize);
+// curl_close ($zipIntilize);
+//
+// $destination = "./downloaded.zip";
+// $fileUsed = fopen($destination, "w+");
+// fputs($fileUsed, $data);
+// fclose($fileUsed);
 
+$zFile = new ZipArchive;
+if ( $zFile->open('CopyFile.zip') === TRUE){
+  echo $zFile->getFromIndex(0);
+  $zFile->close();
+} else {
+  echo 'failed';
+}
 
 ?>
